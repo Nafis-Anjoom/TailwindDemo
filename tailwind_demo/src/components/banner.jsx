a0c311977ef7f9fa7eb5fa778a693536493af414
Nafis-Anjoom <nafisanjoom@gmail.com>
2022-04-24 11:26:34 -0700

    improve banner appearances

--- tailwind_demo/src/components/banner.jsx
+++ tailwind_demo/src/components/banner.jsx
@@ -1,7 +1,14 @@
+import tupac from "../images/tupac.jpg";
+import { GoVerified } from "react-icons/go";
+
 export default function Banner() {
     return(
-        <div className="bg-black h-64">
-            
+        <div className="flex bg-cover bg-center h-80 align-baseline" style={{backgroundImage: `url(${tupac})`}}>
+            <div className="pl-8 pb-8 self-end">
+                <div className="flex items-center"><GoVerified /><span className="px-3">Verified Artist</span></div>
+                <div className="text-8xl font-extrabold">Pusha T</div>
+                <div className="my-2">4,135,714 monthly listeners</div>
+            </div>
         </div>
     );
 }
